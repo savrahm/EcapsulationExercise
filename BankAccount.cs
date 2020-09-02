9f121303703746e1d27b1d1c6f256dc13ac18f4f
Sarah <sarah.b.avram@gmail.com>
2020-09-02 11:12:31 -0400

    initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+
+    class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account1 = new BankAccount();
+
+            account1.Deposit(87.15);
+            Console.WriteLine(account1.GetBalance());
+        }
+    }
+}
